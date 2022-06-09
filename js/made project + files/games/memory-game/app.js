8d3dcab2c2177f945d514091e6e15b96e38c40a1
rk1091 <riya_kapoor@hotmail.com>
2022-06-10 01:13:42 +0530

    Game2 initial files

---
+++ js/made project + files/games/memory-game/app.js
@@ -0,0 +1,92 @@
+const cardArray = [{
+        name: 'rk',
+        img: 'imgs/rk!.jpeg',
+
+    },
+    {
+        name: 'vscode',
+        img: 'imgs/vscode.png',
+
+    },
+    {
+        name: 'ezio',
+        img: 'imgs/ezio.png',
+
+    },
+    {
+        name: 'view',
+        img: 'imgs/708943.png',
+
+    },
+    {
+        name: 'spectre',
+        img: 'imgs/spectre.jpg',
+
+    },
+    {
+        name: 'ca',
+        img: 'imgs/chase atlantic.png',
+
+    },
+    {
+        name: 'rk',
+        img: 'imgs/rk!.jpeg',
+
+    },
+    {
+        name: 'vscode',
+        img: 'imgs/vscode.png',
+
+    },
+    {
+        name: 'ezio',
+        img: 'imgs/ezio.png',
+
+    },
+    {
+        name: 'view',
+        img: 'imgs/708943.png',
+
+    },
+    {
+        name: 'spectre',
+        img: 'imgs/spectre.jpg',
+
+    },
+    {
+        name: 'ca',
+        img: 'imgs/chase atlantic.png',
+
+    }
+
+]
+cardArray.sort(() => 0.5 - Math.random()) //more on this
+
+// console.log(cardArray);
+
+const grid = document.querySelector('#grid') //div ki id aise search karta q.selector
+
+// console.log(grid);
+
+function creatBoard() {
+    for (let i = 0; i < 10; i++) {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// match remains and back wala
+//, {
+//     name: 'coder',
+//     img: 'imgs/coder(back).png',
+
+// }
