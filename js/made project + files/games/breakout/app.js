aae754268c3b0e14b034e6e76cf53b653f166d1e
rk1091 <riya_kapoor@hotmail.com>
2022-06-24 04:44:36 +0530

    game 4 breakout
    super cool initial files

---
+++ js/made project + files/games/breakout/app.js
@@ -0,0 +1,135 @@
+const grid = document.querySelector('.grid') //class created with js if it didnt exist
+
+const blockWidth = 100
+const blockHeight = 20
+const boardWidth = 560
+const dia = 20
+let timerId
+let xDir = 2
+let yDir = 2
+
+const userStart = [230, 10]
+let curr = userStart
+
+const ballStart = [270, 35]
+let bcurr = ballStart
+    //create block indi class
+
+class block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis]
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+        this.topLeft = [xAxis, yAxis + blockHeight]
+    }
+}
+
+//all my blocks
+const blocks = [
+    new block(10, 270),
+    new block(120, 270),
+    new block(230, 270),
+    new block(340, 270),
+    new block(450, 270),
+
+    new block(10, 240),
+    new block(120, 240),
+    new block(230, 240),
+    new block(340, 240),
+    new block(450, 240),
+
+    new block(10, 210),
+    new block(120, 210),
+    new block(230, 210),
+    new block(340, 210),
+    new block(450, 210)
+
+]
+console.log(blocks[0]);
+//draw all my blocks
+function addblocks() {
+
+    for (let i = 0; i < blocks.length; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.style.left = blocks[i].bottomLeft[0] + 'px'
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px'
+        grid.appendChild(block)
+
+    }
+}
+
+addblocks()
+
+
+//draw user
+function drawUser() {
+    user.style.left = curr[0] + 'px'
+    user.style.bottom = curr[1] + 'px'
+}
+
+//add user
+
+const user = document.createElement('div')
+user.classList.add('user')
+drawUser()
+grid.appendChild(user)
+
+//move user
+
+function moveUser(e) {
+    switch (e.key) {
+        case 'ArrowLeft':
+            if (curr[0] > 0) {
+                curr[0] -= 10
+                drawUser()
+            }
+            break;
+        case 'ArrowRight':
+            if (curr[0] < boardWidth - blockWidth) {
+                curr[0] += 10
+                drawUser()
+            }
+            break;
+            // default:
+            //     break;
+    }
+}
+
+document.addEventListener('keydown', moveUser)
+
+//draw ball
+function drawBall() {
+    ball.style.left = bcurr[0] + 'px'
+    ball.style.bottom = bcurr[1] + 'px'
+}
+
+//add ball
+
+const ball = document.createElement('div')
+ball.classList.add('ball')
+drawBall()
+grid.appendChild(ball)
+
+
+//move ball
+function moveBall() {
+    bcurr[0] += xDir
+    bcurr[1] -= yDir
+    drawBall()
+
+}
+
+timerId = setInterval(moveBall, 30);
+
+//check for collisions
+
+fucn
+
+
+function check() {
+    //check for wall collide
+    if (bcurr[0] >= (boardWidth - dia)) {
+        changeDir()
+    }
+}
