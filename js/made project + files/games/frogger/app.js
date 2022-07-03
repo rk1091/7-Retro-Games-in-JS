5c7ca839435cdba020daf9c51506614841944d03
rk1091 <riya_kapoor@hotmail.com>
2022-07-04 03:01:54 +0530

    additions
    this game is really fun to make & play!

--- js/made project + files/games/frogger/app.js
+++ js/made project + files/games/frogger/app.js
@@ -3,8 +3,34 @@ const result = document.querySelector('#result')
 const btn = document.querySelector('#start-pause-btn')
 const sqr = document.querySelectorAll('.grid div')
 
-function moveFrog() {
+let curr = 76
+const width = 9
 
+function moveFrog(e) {
+
+    sqr[curr].classList.remove('frog')
+    switch (e.key) {
+        case 'ArrowLeft':
+            if (curr % width !== 0)
+                curr -= 1
+            break
+        case 'ArrowRight':
+            if (curr % width < width - 1)
+                curr += 1
+            break
+        case 'ArrowUp':
+            if (curr - width >= 0)
+                curr -= width;
+            break
+        case 'ArrowDown':
+            if (curr + width < width * width)
+                curr += width;
+            break
+
+
+    }
+
+    sqr[curr].classList.add('frog')
 }
 
 document.addEventListener('keyup', moveFrog)
