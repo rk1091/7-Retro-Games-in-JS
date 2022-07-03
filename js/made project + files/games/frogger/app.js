16a62e4c8d1ff1841e48c875aa74e0ea2e9f65b8
rk1091 <riya_kapoor@hotmail.com>
2022-07-03 23:53:57 +0530

    game 5 beginning

---
+++ js/made project + files/games/frogger/app.js
@@ -0,0 +1,10 @@
+const timeLeft = document.querySelector('#time-left')
+const result = document.querySelector('#result')
+const btn = document.querySelector('#start-pause-btn')
+const sqr = document.querySelectorAll('.grid div')
+
+function moveFrog() {
+
+}
+
+document.addEventListener('keyup', moveFrog)
