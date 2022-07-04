bab1fb4f86e9f28ddfbfef3d293c06fba2133cb9
rk1091 <riya_kapoor@hotmail.com>
2022-07-05 02:26:00 +0530

    game 7

---
+++ js/made project + files/games/space-invaders/app.js
@@ -0,0 +1,22 @@
+const grid = document.querySelector('.grid')
+    // const res = document.querySelector('#result')
+
+
+for (let i = 0; i < 225; i++) {
+    const sqr = document.createElement('div')
+    grid.appendChild(sqr)
+}
+
+const sqr = Array.from(document.querySelectorAll('.grid div'))
+
+const alienInvaders = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+]
+
+function draw() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+
+    }
+}
